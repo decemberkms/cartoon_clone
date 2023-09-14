1c3598f5a840b986a2dd5b01d69c64646c66554f
Min <decemberkms@gmail.com>
2023-09-14 20:35:04 +0200

    Future

--- app03/lib/services/api_service.dart
+++ app03/lib/services/api_service.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
+import 'package:app03/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  List<WebToonModel> webtooninstances = [];
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebToonModel>> getTodaysToons() async {
     final url = Uri.parse("$baseUrl/$today");
     final reponse = await http.get(url);
     if (reponse.statusCode == 200) {
-      print(reponse.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(reponse.body);
+      for (var webtoon in webtoons) {
+        final toon = WebToonModel.fromJson(webtoon);
+        webtooninstances.add(toon);
+        print(webtooninstances);
+      }
+      return webtooninstances;
     }
     throw Error();
   }
