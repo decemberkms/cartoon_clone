86dedb2027637b8e769bd6c29d96284618da4032
Min <decemberkms@gmail.com>
2023-09-16 21:24:30 +0200

    boxShadow for BoxDecoration in Container

--- app03/lib/screens/home_screen.dart
+++ app03/lib/screens/home_screen.dart
@@ -62,6 +62,13 @@ ListView makeList(AsyncSnapshot<List<WebToonModel>> snapshot) {
             clipBehavior: Clip.hardEdge,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 7,
+                  offset: const Offset(10, 10),
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ],
             ),
             width: 250,
             child: Image.network(
