a81e5a9f80e0f7e8f9fd8c9a831b6491ab6ed041
Min <decemberkms@gmail.com>
2023-09-17 15:19:27 +0200

    Extract widget

--- app03/lib/screens/home_screen.dart
+++ app03/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app03/models/webtoon_model.dart';
 import 'package:app03/services/api_service.dart';
+import 'package:app03/widgets/webtoon_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -45,7 +46,6 @@ class HomeScreen extends StatelessWidget {
 }
 
 ListView makeList(AsyncSnapshot<List<WebToonModel>> snapshot) {
-  print(snapshot.data![1].thumb);
   return ListView.separated(
     padding: const EdgeInsets.symmetric(
       horizontal: 20,
@@ -60,40 +60,10 @@ ListView makeList(AsyncSnapshot<List<WebToonModel>> snapshot) {
     },
     itemBuilder: (context, index) {
       var webtoon = snapshot.data![index];
-      return Column(
-        children: [
-          Container(
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              ],
-            ),
-            width: 250,
-            child: Image.network(
-              webtoon.thumb,
-              headers: const {
-                'User-Agent':
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-                'Referer': 'https://comic.naver.com',
-              },
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            webtoon.title,
-            style: const TextStyle(
-              fontSize: 22,
-            ),
-          ),
-        ],
+      return WebToon.indivisual(
+        id: webtoon.id,
+        title: webtoon.title,
+        thumb: webtoon.thumb,
       );
     },
   );
