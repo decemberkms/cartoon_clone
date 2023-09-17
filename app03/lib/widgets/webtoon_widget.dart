127b103acdc5510c9cfed23f3dacf44ebfa9740c
Min <decemberkms@gmail.com>
2023-09-17 15:22:22 +0200

    GestureDetector

--- app03/lib/widgets/webtoon_widget.dart
+++ app03/lib/widgets/webtoon_widget.dart
@@ -8,40 +8,45 @@ class WebToon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15,
-                offset: const Offset(10, 10),
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        print("Tapped");
+      },
+      child: Column(
+        children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15,
+                  offset: const Offset(10, 10),
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ],
+            ),
+            width: 250,
+            child: Image.network(
+              thumb,
+              headers: const {
+                'User-Agent':
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                'Referer': 'https://comic.naver.com',
+              },
+            ),
           ),
-          width: 250,
-          child: Image.network(
-            thumb,
-            headers: const {
-              'User-Agent':
-                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-              'Referer': 'https://comic.naver.com',
-            },
+          const SizedBox(
+            height: 10,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22,
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
